d8bdae6aedefb0506d78a348ca3b142e7003b839
Cleoir Diego Canei <cleoir.canei@parceiros.odontoprev.com.br>
2023-03-14 19:28:27 -0300

    Implementation feature input audio in anything language to output in audio pt br

--- Domain/Entities/Whisper/ResponseWhisper.cs
+++ Domain/Entities/Whisper/ResponseWhisper.cs
@@ -28,6 +28,7 @@ namespace Domain.Entities.Whisper
         [JsonPropertyName("started_at")]
         public object started_at { get; set; }
         public bool IsSuccess { get; set; }
+        public string SuccessMessage { get; set; }
         public string ErrorMessage { get; set; }
     }
 }
